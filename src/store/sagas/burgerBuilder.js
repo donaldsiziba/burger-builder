2fd180d1a528b3213f4217ae3ff9e4ed06c88fe9
donald <donald@awesomatic.co.za>
2019-06-11 22:05:41 +0200

    initial commit

---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,13 @@
+import {  put } from 'redux-saga/effects';
+import axios from 'axios';
+
+import * as actions from '../actions/index'
+
+export function* initIngredientsSaga(action) {
+    try {
+        const response = yield axios.get('https://burger-builder-760df.firebaseio.com/ingredients.json');
+        yield put(actions.setIngredients(response.data));
+    } catch(error) {
+        yield put(actions.fetchIngredientsFailed());
+    }
+}
